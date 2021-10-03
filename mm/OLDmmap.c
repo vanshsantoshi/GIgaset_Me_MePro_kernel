a2c68a80272082b91292d5500161226ec40ded4b
vanshsantoshi <vanshsantoshi3345@gmail.com>
2021-10-03 23:36:23 +0530

    Changes

--- mm/OLDmmap.c
+++ mm/OLDmmap.c
@@ -213,8 +213,7 @@ static void __remove_shared_vm_struct(struct vm_area_struct *vma,
 	if (vma->vm_flags & VM_DENYWRITE)
 		atomic_inc(&file_inode(file)->i_writecount);
 	if (vma->vm_flags & VM_SHARED)
-		/*mapping->i_mmap_writable--; */
-		mapping_unmap_writable(mapping);
+		mapping->i_mmap_writable--;
 
 	flush_dcache_mmap_lock(mapping);
 	if (unlikely(vma->vm_flags & VM_NONLINEAR))
@@ -615,8 +614,7 @@ static void __vma_link_file(struct vm_area_struct *vma)
 		if (vma->vm_flags & VM_DENYWRITE)
 			atomic_dec(&file_inode(file)->i_writecount);
 		if (vma->vm_flags & VM_SHARED)
-			/* mapping->i_mmap_writable++; */
-			atomic_inc(&mapping->i_mmap_writable);
+			mapping->i_mmap_writable++;
 
 		flush_dcache_mmap_lock(mapping);
 		if (unlikely(vma->vm_flags & VM_NONLINEAR))
@@ -1210,7 +1208,7 @@ unsigned long do_mmap_pgoff(struct file *file, unsigned long addr,
 			unsigned long *populate)
 {
 	struct mm_struct * mm = current->mm;
-	/*struct inode *inode; */
+	struct inode *inode;
 	vm_flags_t vm_flags;
 
 	*populate = 0;
@@ -1273,10 +1271,9 @@ unsigned long do_mmap_pgoff(struct file *file, unsigned long addr,
 			return -EAGAIN;
 	}
 
-	/*inode = file ? file_inode(file) : NULL; */
+	inode = file ? file_inode(file) : NULL;
 
 	if (file) {
-		struct inode *inode = file_inode(file);
 		switch (flags & MAP_TYPE) {
 		case MAP_SHARED:
 			if ((prot&PROT_WRITE) && !(file->f_mode&FMODE_WRITE))
@@ -1311,10 +1308,6 @@ unsigned long do_mmap_pgoff(struct file *file, unsigned long addr,
 
 			if (!file->f_op || !file->f_op->mmap)
 				return -ENODEV;
-
-			if (vm_flags & (VM_GROWSDOWN|VM_GROWSUP))
-				return -EINVAL;
-
 			break;
 
 		default:
@@ -1323,8 +1316,6 @@ unsigned long do_mmap_pgoff(struct file *file, unsigned long addr,
 	} else {
 		switch (flags & MAP_TYPE) {
 		case MAP_SHARED:
-			if (vm_flags & (VM_GROWSDOWN|VM_GROWSUP))
-				return -EINVAL;
 			/*
 			 * Ignore pgoff.
 			 */
@@ -1489,11 +1480,11 @@ unsigned long mmap_region(struct file *file, unsigned long addr,
 {
 	struct mm_struct *mm = current->mm;
 	struct vm_area_struct *vma, *prev;
-	/*int correct_wcount = 0; */
+	int correct_wcount = 0;
 	int error;
 	struct rb_node **rb_link, *rb_parent;
 	unsigned long charged = 0;
-	/*struct inode *inode =  file ? file_inode(file) : NULL; */
+	struct inode *inode =  file ? file_inode(file) : NULL;
 
 	/* Check against address space limit. */
 	if (!may_expand_vm(mm, len >> PAGE_SHIFT)) {
@@ -1558,29 +1549,17 @@ munmap_back:
 	vma->vm_pgoff = pgoff;
 	INIT_LIST_HEAD(&vma->anon_vma_chain);
 
-	/*error = -EINVAL;	 when rejecting VM_GROWSDOWN|VM_GROWSUP */
+	error = -EINVAL;	/* when rejecting VM_GROWSDOWN|VM_GROWSUP */
 
 	if (file) {
-		/*if (vm_flags & (VM_GROWSDOWN|VM_GROWSUP))
-			goto free_vma; */
+		if (vm_flags & (VM_GROWSDOWN|VM_GROWSUP))
+			goto free_vma;
 		if (vm_flags & VM_DENYWRITE) {
 			error = deny_write_access(file);
 			if (error)
 				goto free_vma;
-			/*correct_wcount = 1; */
+			correct_wcount = 1;
 		}
-		if (vm_flags & VM_SHARED) {
-			error = mapping_map_writable(file->f_mapping);
-			if (error)
-				goto allow_write_and_free_vma;
-		}
-
-		/* ->mmap() can change vma->vm_file, but must guarantee that
-		* vma_link() below can deny write-access if VM_DENYWRITE is set
-		* and map writably if VM_SHARED is set. This usually means the
-		* new file must not have been exposed to user-space, yet.
-		*/
-
 		vma->vm_file = get_file(file);
 		error = file->f_op->mmap(file, vma);
 		if (error)
@@ -1599,8 +1578,8 @@ munmap_back:
 		pgoff = vma->vm_pgoff;
 		vm_flags = vma->vm_flags;
 	} else if (vm_flags & VM_SHARED) {
-		/*if (unlikely(vm_flags & (VM_GROWSDOWN|VM_GROWSUP)))
-			goto free_vma; */
+		if (unlikely(vm_flags & (VM_GROWSDOWN|VM_GROWSUP)))
+			goto free_vma;
 		error = shmem_zero_setup(vma);
 		if (error)
 			goto free_vma;
@@ -1622,22 +1601,11 @@ munmap_back:
 	}
 
 	vma_link(mm, vma, prev, rb_link, rb_parent);
-	/* file = vma->vm_file; */
-
-	/* if (correct_wcount)
-		atomic_inc(&inode->i_writecount); */
-	/* Once vma denies write, undo our temporary denial count */
-
-	/*if (vm_flags & VM_DENYWRITE)
-		allow_write_access(file); */
-	 if (file) {
-		if (vm_flags & VM_SHARED)
-			mapping_unmap_writable(file->f_mapping);
-		if (vm_flags & VM_DENYWRITE)
-			allow_write_access(file);
-	}
 	file = vma->vm_file;
 
+	/* Once vma denies write, undo our temporary denial count */
+	if (correct_wcount)
+		atomic_inc(&inode->i_writecount);
 out:
 	perf_event_mmap(vma);
 
@@ -1656,21 +1624,14 @@ out:
 	return addr;
 
 unmap_and_free_vma:
-	/*if (correct_wcount)
+	if (correct_wcount)
 		atomic_inc(&inode->i_writecount);
-	if (vm_flags & VM_DENYWRITE)
-		allow_write_access(file); */
 	vma->vm_file = NULL;
 	fput(file);
 
 	/* Undo any partial mapping done by a device driver. */
 	unmap_region(mm, vma, prev, vma->vm_start, vma->vm_end);
 	charged = 0;
-	if (vm_flags & VM_SHARED)
-		mapping_unmap_writable(file->f_mapping);
-allow_write_and_free_vma:
-	if (vm_flags & VM_DENYWRITE)
-		allow_write_access(file);
 free_vma:
 	kmem_cache_free(vm_area_cachep, vma);
 unacct_error:
@@ -2424,7 +2385,7 @@ detach_vmas_to_be_unmapped(struct mm_struct *mm, struct vm_area_struct *vma,
 static int __split_vma(struct mm_struct * mm, struct vm_area_struct * vma,
 	      unsigned long addr, int new_below)
 {
-	/* struct mempolicy *pol; */
+	struct mempolicy *pol;
 	struct vm_area_struct *new;
 	int err = -ENOMEM;
 
@@ -2448,14 +2409,12 @@ static int __split_vma(struct mm_struct * mm, struct vm_area_struct * vma,
 		new->vm_pgoff += ((addr - vma->vm_start) >> PAGE_SHIFT);
 	}
 
-	/* pol = mpol_dup(vma_policy(vma));
+	pol = mpol_dup(vma_policy(vma));
 	if (IS_ERR(pol)) {
-		err = PTR_ERR(pol); */
-	err = vma_dup_policy(vma, new);
-	if (err)
+		err = PTR_ERR(pol);
 		goto out_free_vma;
-	/*}
-	vma_set_policy(new, pol); */
+	}
+	vma_set_policy(new, pol);
 
 	if (anon_vma_clone(new, vma))
 		goto out_free_mpol;
@@ -2483,8 +2442,7 @@ static int __split_vma(struct mm_struct * mm, struct vm_area_struct * vma,
 		fput(new->vm_file);
 	unlink_anon_vmas(new);
  out_free_mpol:
-	/*mpol_put(pol);*/
-	mpol_put(vma_policy(new));
+	mpol_put(pol);
  out_free_vma:
 	kmem_cache_free(vm_area_cachep, new);
  out_err:
@@ -2827,7 +2785,7 @@ struct vm_area_struct *copy_vma(struct vm_area_struct **vmap,
 	struct mm_struct *mm = vma->vm_mm;
 	struct vm_area_struct *new_vma, *prev;
 	struct rb_node **rb_link, *rb_parent;
-	/*struct mempolicy *pol;*/
+	struct mempolicy *pol;
 	bool faulted_in_anon_vma = true;
 
 	/*
@@ -2873,11 +2831,10 @@ struct vm_area_struct *copy_vma(struct vm_area_struct **vmap,
 			new_vma->vm_start = addr;
 			new_vma->vm_end = addr + len;
 			new_vma->vm_pgoff = pgoff;
-			/*pol = mpol_dup(vma_policy(vma));
-			if (IS_ERR(pol))*/
-			if (vma_dup_policy(vma, new_vma))
+			pol = mpol_dup(vma_policy(vma));
+			if (IS_ERR(pol))
 				goto out_free_vma;
-			/*vma_set_policy(new_vma, pol);*/
+			vma_set_policy(new_vma, pol);
 			INIT_LIST_HEAD(&new_vma->anon_vma_chain);
 			if (anon_vma_clone(new_vma, vma))
 				goto out_free_mempol;
@@ -2892,8 +2849,7 @@ struct vm_area_struct *copy_vma(struct vm_area_struct **vmap,
 	return new_vma;
 
  out_free_mempol:
-	/*mpol_put(pol);*/
-	mpol_put(vma_policy(new_vma));
+	mpol_put(pol);
  out_free_vma:
 	kmem_cache_free(vm_area_cachep, new_vma);
 	return NULL;
